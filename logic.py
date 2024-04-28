2584d35acc6fbd2e3b910412c7b9ed6ae2543517
Arman <arman.aghababyan.1301@gmail.com>
2024-04-28 00:41:09 -0700

    PokemonLVL2

--- logic.py
+++ logic.py
@@ -66,6 +66,15 @@ class Pokemon:
          bot.reply_to(message, """\
             У твоего покемона {self.hp} жизни и {self.power} силы\
             """)
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.current()  
+        delta_time = timedelete(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"  
 class Wizard(Pokemon):
     def info(self):
         return 'У тебя покемон волшебник \n\n' + super().info()
