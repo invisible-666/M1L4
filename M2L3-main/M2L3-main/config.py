c9fcde0bb9279a23e68f033578ba09c6b2ddd96d
Arman <arman.aghababyan.1301@gmail.com>
2024-05-09 02:09:52 -0700

    Check here

--- M2L3-main/M2L3-main/config.py
+++ M2L3-main/M2L3-main/config.py
@@ -1,1 +1,2 @@
-token = "7058753529:AAHEKGkWEwclTCOM6W8VG-AunOU-fu1QzpI"
+Token = "7058753529:AAHEKGkWEwclTCOM6W8VG-AunOU-fu1QzpI"
+#This is my bot's token
--- config.py
+++
@@ -1,2 +0,0 @@
-Token = "7058753529:AAHEKGkWEwclTCOM6W8VG-AunOU-fu1QzpI"
-#This is my bot's token
